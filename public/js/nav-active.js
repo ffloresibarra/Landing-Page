71b7bf9436612c34a4350bda9a5538f8c3730c47
chernandezUV <chernandez@unitedvirtualities>
2019-11-25 22:02:39 -0700

    1.- Features Finished

--- public/js/nav-active.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("a").click(function() {
-       // remove classes from all
-       $("a").removeClass("active");
-       // add class to the one we clicked
-       $(this).addClass("active");
-    });
- });
