381330aebb396aec081d37c3ec103ff5ef7c5e9a
Eric-UV <edelgadillo@unitedvirtualities.com>
2019-11-28 09:49:20 -0700

    before merge

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    key: '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',
+    recipient: 'edelgadillo@unitedvirtualities.com',
+    templateId: 'd-edce38ea4428474382cd9f74ef084fa6'
+}
